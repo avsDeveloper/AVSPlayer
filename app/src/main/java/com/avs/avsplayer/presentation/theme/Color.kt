65d90111824a2cdabe9f9c73da88f995034fdfda
al <alvig777@gmail.com>
2024-01-06 13:19:17 +0300

    Colors changed

--- app/src/main/java/com/avs/avsplayer/presentation/theme/Color.kt
+++ app/src/main/java/com/avs/avsplayer/presentation/theme/Color.kt
@@ -3,9 +3,9 @@ package com.avs.avsplayer.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 val md_theme_dark_onPrimary = Color(0xFF353100)
-val md_theme_dark_primary = Color(0xFFD4CA51)
-val md_theme_dark_surface = Color(0xFF1D1C16)
-val md_theme_dark_onSurface = Color(0xFFE6E2D9)
-val md_theme_dark_surfaceVariant = Color(0xFF49473A)
-val md_theme_dark_onSurfaceVariant = Color(0xFFCBC7B5)
+val md_theme_dark_primary = Color(0xFFb6ccb8)
+val md_theme_dark_surface = Color(0xFF191c1b)
+val md_theme_dark_onSurface = Color(0xFFe1e3de)
+val md_theme_dark_surfaceVariant = Color(0xFF414941)
+val md_theme_dark_onSurfaceVariant = Color(0xFFc1c9bf)
 
